c07b224ee5f8a9fd7f4bedeff84ead77e18d8915
5-6 <5-6@DESKTOP-7NJIOCU>
2022-06-22 21:24:00 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicGame
+{
+    class MagicGame
+    {
+        public int SecretNum, Start = 0, Finish = 100;
+        public void StepLeft()
+        {
+            
+        }
+        public void StepRigth()
+        {
+
+        }
+        public int IsNumCorrect(ref int SecretNum)
+        {
+            int _result = 0;
+            Console.WriteLine("Это число {0}?\n(1 - меньше, 2 - больше, другое число - я угадал)", SecretNum);
+            do
+            {
+                Console.WriteLine("Введите число: ");
+                try
+                    {
+                        _result = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Не могу распознать ответ. Введите, пожалуйста, заново...");
+                    }
+            } while (_result == 0);
+
+
+            if (_result == 1)
+            {
+                StepLeft();
+            }
+            else if (_result == 2)
+                {
+                    StepRigth();
+                }
+                else
+                    GameOver();
+            
+            return _result;
+        }
+        public void GameOver()
+        {
+            Console.WriteLine("Congratulations!\n\n");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Игра начата!");
+            MagicGame myGame = new MagicGame();
+            myGame.Start = 0;
+            myGame.Finish = 100;
+            myGame.SecretNum = (myGame.Start + myGame.Finish) / 2;
+            myGame.IsNumCorrect(ref myGame.SecretNum);
+            myGame.StepLeft();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MagicGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MagicGame")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("056764fb-2a98-4c91-aeab-1d450593426b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
